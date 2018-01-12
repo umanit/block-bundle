b6f4538987c290da589f109c143ac20e994f04fb
Arthur Guigand <aguigand@umanit.fr>
2018-01-12 10:52:52 +0100

    Silent exeption on block rendering
    If debug is not enabled, any exception occuring during block rendering will be silented and logged.
    This is to avoid 500 in prod.

--- Twig/BlockExtension.php
+++ Twig/BlockExtension.php
@@ -2,6 +2,7 @@
 
 namespace Umanit\BlockBundle\Twig;
 
+use Psr\Log\LoggerInterface;
 use Umanit\BlockBundle\Model\BlockInterface;
 use Umanit\BlockBundle\Resolver\BlockManagerResolver;
 
@@ -14,15 +15,30 @@ class BlockExtension extends \Twig_Extension
      * @var BlockManagerResolver
      */
     private $blockManagerResolver;
+    /**
+     * @var bool
+     */
+    private $debugIsEnabled;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * BlockExtension constructor.
      *
      * @param BlockManagerResolver $blockManagerResolver
+     * @param bool                 $debugIsEnabled
+     * @param LoggerInterface      $logger
      */
-    public function __construct(BlockManagerResolver $blockManagerResolver)
-    {
+    public function __construct(
+        BlockManagerResolver $blockManagerResolver,
+        LoggerInterface $logger,
+        $debugIsEnabled = false
+    ) {
         $this->blockManagerResolver = $blockManagerResolver;
+        $this->logger               = $logger;
+        $this->debugIsEnabled       = $debugIsEnabled;
     }
 
     /**
@@ -43,11 +59,23 @@ class BlockExtension extends \Twig_Extension
      * @param BlockInterface $block
      *
      * @return mixed
+     * @throws \Exception
      */
     public function renderBlock(BlockInterface $block)
     {
         $blockManager = $this->blockManagerResolver->resolveManager($block);
+        $html         = '';
 
-        return $blockManager->render($block);
+        try {
+            $html = $blockManager->render($block);
+        } catch (\Exception $e) {
+            if (true === $this->debugIsEnabled) {
+                throw $e;
+            }
+            // If debug is not enabled (prod), silent the exception and log
+            $this->logger->critical($e->getMessage());
+        }
+
+        return $html;
     }
 }
