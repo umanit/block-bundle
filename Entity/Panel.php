c8b8f456d3996040b405ef8f3ff9e16d9a665a65
jodou <jdouet@umanit.fr>
2023-05-22 10:43:59 +0200

    interface PanelInterface - correction des méthodes du panel

--- Entity/Panel.php
+++ Entity/Panel.php
@@ -79,7 +79,7 @@ class Panel implements PanelInterface
         return $this->blocks;
     }
 
-    public function setBlocks(ArrayCollection $blocks = null): Panel
+    public function setBlocks(Collection $blocks = null): PanelInterface
     {
         $this->blocks = $blocks;
 
