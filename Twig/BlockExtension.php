0b8ac6b831082937d6741ac6c557a2509d419857
Vivien DE BONA <vdebona@umanit.fr>
2026-07-22 10:43:41 +0200

    chore: remove dead code

--- Twig/BlockExtension.php
+++ Twig/BlockExtension.php
@@ -4,43 +4,11 @@ declare(strict_types=1);
 
 namespace Umanit\BlockBundle\Twig;
 
-use Psr\Log\LoggerInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
-use Umanit\BlockBundle\Resolver\BlockManagerResolver;
 
 class BlockExtension extends AbstractExtension
 {
-    /**
-     * @var BlockManagerResolver
-     */
-    private $blockManagerResolver;
-    /**
-     * @var bool
-     */
-    private $debugIsEnabled;
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
-     * BlockExtension constructor.
-     *
-     * @param BlockManagerResolver $blockManagerResolver
-     * @param bool                 $debugIsEnabled
-     * @param LoggerInterface      $logger
-     */
-    public function __construct(
-        BlockManagerResolver $blockManagerResolver,
-        LoggerInterface $logger,
-        bool $debugIsEnabled = false
-    ) {
-        $this->blockManagerResolver = $blockManagerResolver;
-        $this->logger = $logger;
-        $this->debugIsEnabled = $debugIsEnabled;
-    }
-
     public function getFunctions(): array
     {
         return [
